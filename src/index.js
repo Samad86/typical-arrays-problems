4d63525bbe2134a26134918e71f766b4efaac01c
samad86 <mads198644@gmail.com>
2021-02-23 12:31:56 +0300

    refactor: add solution if no params specified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 exports.min = function min(array) {
+    if (!array) return 0;
     return array.length ? Math.min(...array) : 0;
 };
 
 exports.max = function max(array) {
+    if (!array) return 0;
     return array.length ? Math.max(...array) : 0;
 };
 
 exports.avg = function avg(array) {
+    if (!array) return 0;
     return array.length
         ? array.reduce((acc, elem) => acc + elem, 0) / array.length
         : 0;
